9f693bb5bb6a68c0910e6b3d02119c90df71c290
Mehran-at <ajaxcube@gmail.com>
2023-02-09 00:48:19 +0100

    initial commit

---
+++ task_one_replace_every_nth/src/main/java/com/kingcode/task_one_replace_every_nth/TaskOneReplaceEveryNthApplication.java
@@ -0,0 +1,13 @@
+package com.kingcode.task_one_replace_every_nth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOneReplaceEveryNthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskOneReplaceEveryNthApplication.class, args);
+	}
+
+}
---
+++ task_one_replace_every_nth/src/test/java/com/kingcode/task_one_replace_every_nth/TaskOneReplaceEveryNthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingcode.task_one_replace_every_nth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskOneReplaceEveryNthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
