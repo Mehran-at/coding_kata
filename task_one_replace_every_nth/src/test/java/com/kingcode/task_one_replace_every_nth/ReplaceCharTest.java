549827937604edf4dd96149b5ca3f3a00f9c19af
Mehran-at <ajaxcube@gmail.com>
2023-02-09 02:55:44 +0100

    Add test

---
+++ task_one_replace_every_nth/src/test/java/com/kingcode/task_one_replace_every_nth/ReplaceCharTest.java
@@ -0,0 +1,18 @@
+package com.kingcode.task_one_replace_every_nth;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReplaceCharTest {
+
+    ReplaceChar replaceChar = new ReplaceChar();
+
+    @Test
+    void checkIfMinusNumberReturnOriginalText() {
+        Input input = new Input("Vader said: No, I am your father!");
+        String expected = replaceChar.replace(input, 'a', 'o', 2);
+        assertEquals(input.getText(), expected);
+    }
+
+}
