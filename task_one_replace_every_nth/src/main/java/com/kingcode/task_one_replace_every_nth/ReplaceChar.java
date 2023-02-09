c6c86625269643a992f536cd6abac28c0bbdfd65
Mehran-at <ajaxcube@gmail.com>
2023-02-09 02:50:30 +0100

    Code refactor

--- task_one_replace_every_nth/src/main/java/com/kingcode/task_one_replace_every_nth/ReplaceChar.java
+++ task_one_replace_every_nth/src/main/java/com/kingcode/task_one_replace_every_nth/ReplaceChar.java
@@ -2,19 +2,26 @@ package com.kingcode.task_one_replace_every_nth;
 
 public class ReplaceChar {
 
-
     public String replace(Input input, Character oldValue, Character newValue, int occurrences) {
-        System.out.println("sssss");
         if (occurrences <= 0 || occurrences > oldValue) return input.getText();
         int times = 0;
         for (int index = 0; index < input.getText().length(); index++) {
-            if (Character.hashCode(input.getText().charAt(index)) == Character.hashCode(oldValue)) {
-                times = times + 1;
-                if (times % occurrences == 0) {
-                    input.setText(input.getText().substring(0, index) + newValue + input.getText().substring(index + 1));
-                }
-            }
+            times = checkIfCharactersAreTheSame(input, oldValue, newValue, occurrences, times, index);
         }
         return input.getText();
+    }
+
+    private int checkIfCharactersAreTheSame(Input input, Character oldValue, Character newValue, int occurrences, int times, int index) {
+        if (Character.hashCode(input.getText().charAt(index)) == Character.hashCode(oldValue)) {
+            times = times + 1;
+            replaceCharIfItsOccurrenceTimes(input, newValue, occurrences, times, index);
+        }
+        return times;
+    }
+
+    private void replaceCharIfItsOccurrenceTimes(Input input, Character newValue, int occurrences, int times, int index) {
+        if (times % occurrences == 0) {
+            input.setText(input.getText().substring(0, index) + newValue + input.getText().substring(index + 1));
+        }
     }
 }
