8203fb14f8edac4fdec002d8f00c668a15b17791
Mehran-at <ajaxcube@gmail.com>
2023-02-10 23:50:15 +0100

    Refactor via remove unused constant

--- task_three_switch_refactor/src/main/java/com/kingcode/ParrotImpl.java
+++ task_three_switch_refactor/src/main/java/com/kingcode/ParrotImpl.java
@@ -4,7 +4,6 @@ public class ParrotImpl implements Parrot {
 
     public static final double BASE_SPEED = 12.0;
     public static final double MAX_NUM = 24.0;
-    public static final double LOAD_FACTOR = 9.0;
     private final ParrotTypeEnum type;
     private final double voltage;
     private final boolean isNailed;
