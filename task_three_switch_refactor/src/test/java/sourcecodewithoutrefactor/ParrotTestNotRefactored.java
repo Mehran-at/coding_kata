c1451901b7eb71204a9c60d03caa48b7df940ba3
Mehran-at <ajaxcube@gmail.com>
2023-02-11 00:07:55 +0100

    Correct not refactored name fields

--- task_three_switch_refactor/src/test/java/sourcecodewithoutrefactor/ParrotTestNotRefactored.java
+++ task_three_switch_refactor/src/test/java/sourcecodewithoutrefactor/ParrotTestNotRefactored.java
@@ -1,6 +1,7 @@
 package sourcecodewithoutrefactor;
 
-import com.kingcode.type.Norwegian;
+import com.kingcode.sourcecodewithoutrefactor.ParrotNotRefactored;
+import com.kingcode.sourcecodewithoutrefactor.ParrotTypeEnumNotRefactored;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,43 +10,43 @@ public class ParrotTestNotRefactored {
 
     @Test
     public void getSpeedOfEuropeanParrot() {
-        Norwegian parrot = new Norwegian(0, false);
+        ParrotNotRefactored parrot = new ParrotNotRefactored(ParrotTypeEnumNotRefactored.EUROPEAN, 0, 0, false);
         assertEquals(12.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_One_Coconut() {
-        Norwegian parrot = new Norwegian(0, false);
+        ParrotNotRefactored parrot = new ParrotNotRefactored(ParrotTypeEnumNotRefactored.AFRICAN, 1, 0, false);
         assertEquals(3.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_Two_Coconuts() {
-        Norwegian parrot = new Norwegian(0, false);
+        ParrotNotRefactored parrot = new ParrotNotRefactored(ParrotTypeEnumNotRefactored.AFRICAN, 2, 0, false);
         assertEquals(0.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_No_Coconuts() {
-        Norwegian parrot = new Norwegian(0, false);
+        ParrotNotRefactored parrot = new ParrotNotRefactored(ParrotTypeEnumNotRefactored.AFRICAN, 0, 0, false);
         assertEquals(12.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_nailed() {
-        Norwegian parrot = new Norwegian(1.5, true);
+        ParrotNotRefactored parrot = new ParrotNotRefactored(ParrotTypeEnumNotRefactored.NORWEGIAN_BLUE, 0, 1.5, true);
         assertEquals(0.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed() {
-        Norwegian parrot = new Norwegian(1.5, false);
+        ParrotNotRefactored parrot = new ParrotNotRefactored(ParrotTypeEnumNotRefactored.NORWEGIAN_BLUE, 0, 1.5, false);
         assertEquals(18.0, parrot.getSpeed(), 0.0);
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
-        Norwegian parrot = new Norwegian(4, false);
+        ParrotNotRefactored parrot = new ParrotNotRefactored(ParrotTypeEnumNotRefactored.NORWEGIAN_BLUE, 0, 4, false);
         assertEquals(24.0, parrot.getSpeed(), 0.0);
     }
 }
