910125d81f56239fefea164e98b669a5ac38547f
Mehran-at <ajaxcube@gmail.com>
2023-02-10 01:59:15 +0100

    Calculating right gravity

--- task_two_gravity_flip/src/main/java/com/kingcode/Main.java
+++ task_two_gravity_flip/src/main/java/com/kingcode/Main.java
@@ -11,5 +11,6 @@ public class Main {
         PlayBoxGravity playBoxGravity = new PlayBoxGravity();
         // must output  [5, 5, 4, 3, 1]
         System.out.println(Arrays.toString(playBoxGravity.flip(new Direction('l'), new Cube(new int[]{1, 4, 5, 3, 5}))));
+        System.out.println(Arrays.toString(playBoxGravity.flip(new Direction('r'), new Cube(new int[]{3, 2, 1, 2}))));
     }
 }
--- task_two_gravity_flip/src/main/java/com/kingcode/gravitylogic/GravityToRight.java
+++ task_two_gravity_flip/src/main/java/com/kingcode/gravitylogic/GravityToRight.java
@@ -3,6 +3,21 @@ package com.kingcode.gravitylogic;
 public class GravityToRight {
 
     public int[] calculateForGravityRight(int[] cubes) {
-        return null;
+        for (int ignored : cubes) {
+            for (int cube = cubes.length-1; cube > 0; cube--) {
+                int columnLeft = cubes[cube - 1];
+                int columnRight = cubes[cube];
+
+                moveCubesToTheRightColumn(cubes, cube, columnLeft, columnRight);
+            }
+        }
+        return cubes;
+    }
+
+    private void moveCubesToTheRightColumn(int[] cubes, int cube, int columnLeft, int columnRight) {
+        if (columnRight < columnLeft) {
+            cubes[cube] = columnRight + (columnLeft - columnRight);
+            cubes[cube - 1] = columnLeft - (columnLeft + columnRight);
+        }
     }
 }
