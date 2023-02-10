72196c10db2be5cb17ca59c48013a96f0775b208
Mehran-at <ajaxcube@gmail.com>
2023-02-10 23:57:56 +0100

    Refactor via removing unused 0.0 which was defined as delta

--- task_three_switch_refactor/src/test/java/ParrotTest.java
+++ task_three_switch_refactor/src/test/java/ParrotTest.java
@@ -8,42 +8,42 @@ public class ParrotTest {
     @Test
     public void getSpeedOfEuropeanParrot() {
         Parrot parrot = new European();
-        assertEquals(12.0, parrot.getSpeed(), 0.0);
+        assertEquals(12.0, parrot.getSpeed());
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_One_Coconut() {
         Parrot parrot = new African(1);
-        assertEquals(3.0, parrot.getSpeed(), 0.0);
+        assertEquals(3.0, parrot.getSpeed());
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_Two_Coconuts() {
         Parrot parrot = new African(2);
-        assertEquals(0.0, parrot.getSpeed(), 0.0);
+        assertEquals(0.0, parrot.getSpeed());
     }
 
     @Test
     public void getSpeedOfAfricanParrot_With_No_Coconuts() {
         Parrot parrot = new African(0);
-        assertEquals(12.0, parrot.getSpeed(), 0.0);
+        assertEquals(12.0, parrot.getSpeed());
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_nailed() {
         Norwegian parrot = new Norwegian(1.5, true);
-        assertEquals(0.0, parrot.getSpeed(), 0.0);
+        assertEquals(0.0, parrot.getSpeed());
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed() {
         Parrot parrot = new Norwegian(1.5, false);
-        assertEquals(18.0, parrot.getSpeed(), 0.0);
+        assertEquals(18.0, parrot.getSpeed());
     }
 
     @Test
     public void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
         Parrot parrot = new Norwegian(4, false);
-        assertEquals(24.0, parrot.getSpeed(), 0.0);
+        assertEquals(24.0, parrot.getSpeed());
     }
 }
