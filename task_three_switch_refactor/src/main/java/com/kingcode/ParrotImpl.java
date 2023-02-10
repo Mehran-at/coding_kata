3d479f2fc2ea9aa7a49894d030fd4fe2569eda88
Mehran-at <ajaxcube@gmail.com>
2023-02-10 23:36:10 +0100

    Refactor with defining BASE_SPEED constant and then inlining it

--- task_three_switch_refactor/src/main/java/com/kingcode/ParrotImpl.java
+++ task_three_switch_refactor/src/main/java/com/kingcode/ParrotImpl.java
@@ -2,6 +2,7 @@ package com.kingcode;
 
 public class ParrotImpl implements Parrot {
 
+    public static final double BASE_SPEED = 12.0;
     private final ParrotTypeEnum type;
     private final int numberOfCoconuts;
     private final double voltage;
@@ -16,22 +17,18 @@ public class ParrotImpl implements Parrot {
 
     public double getSpeed() {
         return switch (type) {
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+            case AFRICAN -> Math.max(0, BASE_SPEED - getLoadFactor() * numberOfCoconuts);
             case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
             default -> throw new IllegalStateException("Unexpected value: " + type);
         };
     }
 
     private double getBaseSpeed(double voltage) {
-        return Math.min(24.0, voltage * getBaseSpeed());
+        return Math.min(24.0, voltage * BASE_SPEED);
     }
 
     private double getLoadFactor() {
         return 9.0;
-    }
-
-    private double getBaseSpeed() {
-        return 12.0;
     }
 
 }
