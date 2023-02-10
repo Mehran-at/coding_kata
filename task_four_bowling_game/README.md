a9731fd1547af91c8bc2ec582e5610780de420b9
Mehran-at <ajaxcube@gmail.com>
2023-02-11 00:52:44 +0100

    Add bowling game kata

---
+++ task_four_bowling_game/README.md
@@ -0,0 +1,4 @@
+# Bowling Game
+- Bowling Game Kata
+
+![Alt text](src/main/resources/bowling_game.png?raw=true "Bowling Game Task")
---
+++ task_four_bowling_game/src/test/java/com/kingcode/MainTest.java
@@ -0,0 +1,12 @@
+package com.kingcode;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void getNothing() {
+
+    }
+
+}
