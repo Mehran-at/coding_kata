8d8320a19f187dad27cd731f87d2906deff7c9ff
Mehran-at <ajaxcube@gmail.com>
2023-02-14 11:37:27 +0100

    Start and finish shelf numbers

--- task_six_cats_and_shelves/src/main/java/com/kingcode/Play.java
+++ task_six_cats_and_shelves/src/main/java/com/kingcode/Play.java
@@ -3,12 +3,24 @@ package com.kingcode;
 public class Play {
 
     public static int solution(int start, int finish) {
-        int jump = start;
-        for (jump = start; jump < finish; jump++) {
-            if (jump + 1 == finish) return jump;
-
-
-        }
+        // 1 -> 2 -> 3 -> 4 -> 5
+        int jump = 1;
+        if (start + jump == finish) return jump;
+        jump = 2;
+        if (jump + 1 == finish || jump + 3 == finish) return jump;
+        jump = 3;
+        if (jump + 3 + 1 == finish) return jump;
         return jump;
     }
 }
+
+//    int jump = start;
+//        for (jump = start; jump < finish;) {
+//        if (jump + 1 == finish) return jump;
+//        jump++;
+//        if (jump + 1 == finish) return jump;
+//        jump++;
+//        if (jump + 1 == finish) return jump;
+//        jump++;
+//
+//        }
