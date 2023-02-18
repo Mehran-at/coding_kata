f257a1c43cec54fc0b65427a68b92dac0363c445
Mehran-at <ajaxcube@gmail.com>
2023-02-18 15:47:49 +0100

    Correct the time-string 2 effort

--- task_9_correct_the_time-string/src/main/java/com/kingcode/Main.java
+++ task_9_correct_the_time-string/src/main/java/com/kingcode/Main.java
@@ -2,7 +2,7 @@ package com.kingcode;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(CorrectTimeString.timeCorrect("52:01:01"));
+        System.out.println(Solution.timeCorrect("02:00:0"));
         System.out.println("Hello world!");
     }
 }
---
+++ task_9_correct_the_time-string/src/main/java/com/kingcode/Solution.java
@@ -0,0 +1,37 @@
+package com.kingcode;
+
+public class Solution {
+
+    public static String timeCorrect(String timestring) {
+
+        if (timestring == null) return null;
+        if (timestring.equals("")) return "";
+        if (checkForInvalidFormat(timestring) == null) return null;
+        int hour = getIntValuesHour(timestring);
+        int minute = getIntValuesMinute(timestring);
+        int second = getIntValuesSecond(timestring);
+
+
+        return hour + ":" + minute + ":" + second;
+    }
+
+    private static int getIntValuesHour(String timestring) {
+        return Integer.parseInt(timestring.substring(0, 2));
+    }
+
+    private static int getIntValuesMinute(String timestring) {
+        return Integer.parseInt(timestring.substring(3, 5));
+    }
+
+    private static int getIntValuesSecond(String timestring) {
+        return Integer.parseInt(timestring.substring(6, 8));
+    }
+
+    private static String checkForInvalidFormat(String timestring) {
+        if (timestring.length() != 8 || timestring.contains(";")) return null;
+        if (!Character.isDigit(timestring.charAt(0)) || !Character.isDigit(timestring.charAt(1))) return null;
+        if (!Character.isDigit(timestring.charAt(3)) || !Character.isDigit(timestring.charAt(4))) return null;
+        if (!Character.isDigit(timestring.charAt(6)) || !Character.isDigit(timestring.charAt(7))) return null;
+        return timestring;
+    }
+}
