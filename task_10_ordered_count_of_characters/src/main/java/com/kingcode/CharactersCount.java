e88974cf78ccd9450e13ed57f0b76085ae12826e
Mehran-at <ajaxcube@gmail.com>
2023-02-22 01:39:36 +0100

    Correct the time-string optimized solution

--- task_10_ordered_count_of_characters/src/main/java/com/kingcode/CharactersCount.java
+++ task_10_ordered_count_of_characters/src/main/java/com/kingcode/CharactersCount.java
@@ -6,18 +6,18 @@ import java.util.List;
 import java.util.Map;
 
 public class CharactersCount {
-    //  abaac
+
     public static List<Map<Character, Integer>> orderedCount(String text) {
         List<Map<Character, Integer>> pairList = new ArrayList<>();
-
+        //  abaac
         Map<Character, Integer> list = new HashMap<>();
-        for (int index = 0; index < text.length(); index++) {
-            int finalIndex = index;
-            int count = (int) text.chars().filter(ch -> ch == text.charAt(finalIndex)).count();
+        int index;
+        for (index = 0; index < text.length(); index++) {
+            int charIndex = index;
+            int count = (int) text.chars().filter(ch -> ch == text.charAt(charIndex)).count();
             list.put(text.charAt(index), count);
         }
         pairList.add(list);
         return pairList;
     }
-
 }
