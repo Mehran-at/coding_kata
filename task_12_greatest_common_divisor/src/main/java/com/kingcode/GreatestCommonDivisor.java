8220d5c44037e485da8bf5e974eee0061216be2c
Mehran-at <ajaxcube@gmail.com>
2023-02-24 23:25:05 +0100

    Euclidean Algorithm

--- task_12_greatest_common_divisor/src/main/java/com/kingcode/GreatestCommonDivisor.java
+++ task_12_greatest_common_divisor/src/main/java/com/kingcode/GreatestCommonDivisor.java
@@ -7,22 +7,32 @@ import java.util.stream.IntStream;
 
 public class GreatestCommonDivisor {
 
-    public static int compute(int x, int y) {
-        ArrayList<Integer> divisorsX = getDivisors(x);
-        ArrayList<Integer> divisorsY = getDivisors(y);
-        Integer largestDivisableNumForBothNumbers = 0;
-        for (Integer value : divisorsX) {
-            for (Integer integer : divisorsY) {
-                if (Objects.equals(value, integer)) {
-                    largestDivisableNumForBothNumbers = value;
-                    break;
-                }
-            }
-        }
-        return largestDivisableNumForBothNumbers;
-    }
+//    public static int compute(int x, int y) {
+//        ArrayList<Integer> divisorsX = getDivisors(x);
+//        System.out.println(divisorsX);
+//        ArrayList<Integer> divisorsY = getDivisors(y);
+//        System.out.println(divisorsY);
+//        Integer largestDivisibleNumForBothNumbers = 0;
+//        for (Integer valueX : divisorsX) {
+//            for (Integer valueY : divisorsY) {
+//                if (Objects.equals(valueX, valueY)) {
+//                    largestDivisibleNumForBothNumbers = valueX;
+//                    break;
+//                }
+//            }
+//        }
+//        return largestDivisibleNumForBothNumbers;
+//    }
+//
+//    private static ArrayList<Integer> getDivisors(int num) {
+//        return IntStream.rangeClosed(1, num).filter(i -> num % i == 0).boxed().collect(Collectors.toCollection(ArrayList::new));
+//    }
 
-    private static ArrayList<Integer> getDivisors(int num) {
-        return IntStream.rangeClosed(1, num).filter(i -> num % i == 0).boxed().collect(Collectors.toCollection(ArrayList::new));
-    }
+//    public static int compute(int x, int y) {
+//        return (x % y != 0) ? compute(y, x % y) : y;
+//    }//Euclidean Algorithm
+
+    public static int compute(int x, int y) {
+        return (y == 0) ? x : compute(y, x % y);
+    }//Euclidean Algorithm
 }
--- task_12_greatest_common_divisor/src/test/java/com/kingcode/GreatestCommonDivisorTest.java
+++ task_12_greatest_common_divisor/src/test/java/com/kingcode/GreatestCommonDivisorTest.java
@@ -7,6 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class GreatestCommonDivisorTest {
     @Test
     public void testGcd() {
+        assertEquals(50, GreatestCommonDivisor.compute(50,100));
+        assertEquals(1, GreatestCommonDivisor.compute(100,101));
+        assertEquals(101, GreatestCommonDivisor.compute(101,101));
+        assertEquals(6, GreatestCommonDivisor.compute(6,12));
         assertEquals(6, GreatestCommonDivisor.compute(30,12));
         assertEquals(1, GreatestCommonDivisor.compute(8,9));
         assertEquals(1, GreatestCommonDivisor.compute(1,1));
